0356f66f77b5ef995cb3be46e557554fbd78da80
lmizrahi12 <lmizrahi12@gmail.com>
2016-09-01 14:22:52 +0200

    Updated data to give better graphs

--- Lab 3/main.cpp
+++ Lab 3/main.cpp
@@ -11,9 +11,7 @@
 using namespace std;
 
 void MergeSort();
-void Improved_MergeSort_Best();
-void Improved_MergeSort_Worst();
-void Improved_MergeSort_Average();
+void Improved_MergeSort();
 void QuickSort_Worst();
 void QuickSort_Average();
 
@@ -36,22 +34,9 @@ int main(){
 			MergeSort();
 		}
 		
-		/*else if(n == 2){
-			cout << "1. Best Case" << endl;
-        	cout << "2. Worst Case" << endl;
-        	cout << "3. Average Case" << endl;
-        	cin >> n;
-        	
-        	if(n == 1){
-        		//Improved_MergeSort_Best();
-        	}
-        	else if(n == 2){
-        		//Improved_MergeSort_Worst();
-        	}
-        	else if(n == 3){
-        		//Improved_MergeSort_Average();
-        	}
-		}*/
+		else if(n == 2){
+			Improved_MergeSort();
+		}
 	}
 	
 	else if(n == 2){
@@ -89,16 +74,24 @@ void MergeSort(){
     }
 }
 
-void Improved_MergeSort_Best(){
-    //TODO
-}
+void Improved_MergeSort(){
+    ofstream file;
+    vector<int> list;
+    int n;
+    
+    for(n = 1000;n <= 20000;n+=100){
+        vector<int> list(n,0);
+        Random_List(list);
 
-void Improved_MergeSort_Worst(){
-    //TODO
-}
-
-void Improved_MergeSort_Average(){
-    //TODO
+        double start = omp_get_wtime();
+            improved_mergeSort(list, 0,n-1);
+        double end = omp_get_wtime();
+        
+        cout << "List size: " << n << " ,  Sort time: " << end-start << endl;
+        file.open("improved_mergesort_random.csv", ios::app);
+        file << n << "," << end-start << endl;
+        file.close();
+    }
 }
 
 void QuickSort_Worst(){
